e712986470c8eef8264a54b4eeb6a8f2f4c43adc
remithr <remithr@winjit.com>
2022-03-25 08:03:55 +0530

    to json method added

--- test/features/user_authentication/data/models/user_details_model_test.dart
+++ test/features/user_authentication/data/models/user_details_model_test.dart
@@ -17,9 +17,10 @@ void main() {
   //   expect(umodel, isA<UserDetailsModel>());
   // });
 
-  group('Parsing of JSON to dart', () {
+  group('Parsing of JSON to dart object', () {
     test('parsing from json to dart object when ID is integer', () {
       // arrange
+      ///fixture usage: load a file for testing
       final Map<String, dynamic> jsonMap = jsonDecode(fixture('user.json'));
       // act
       final result = UserDetailsModel.fromJson(jsonMap);
@@ -35,6 +36,20 @@ void main() {
       final result = UserDetailsModel.fromJson(jsonMap);
       // assert
       expect(result, umodel);
+    });
+  });
+
+  group('toJSON', () {
+    test('Should return a Map containing all the required keys', () {
+      // act
+      final result = umodel.toJson();
+      // assert
+      final expected = {
+        'userID': '123',
+        'name': 'Remith',
+        'email': 'remithr@yopmail.com'
+      };
+      expect(result, expected);
     });
   });
 }
